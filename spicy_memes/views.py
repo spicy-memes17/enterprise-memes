27ec7968295804e0c311bb82a443224fcc33abff
kr89 <lorant.peter@hotmail.de>
2017-05-27 13:32:37 +0200

    Revert "SignUp reworked, added new deleteAccount"

--- spicy_memes/views.py
+++ spicy_memes/views.py
@@ -1,29 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .forms import SignUpForm
-from .forms import LogInForm
-from .models import MyUser
-from django.contrib.auth import authenticate, login
+from .forms import SignUp
 
 def content(request):
-    current_user = request.user
-    return render(request, 'content.html', {'user' : current_user})
+    return render(request, 'content.html')
 
 def signUp(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.username = request.POST['username']
-            user.email = request.POST['email']
-            user.password = request.POST['password']
-            user.save()
-            return HttpResponseRedirect('/spicy_memes')
+        signUpForm = SignUp(request.POST)
+        if signUpForm.isVaild():
+            # use data
+            return HttpResponseRedirect('/content/')
 
     else:
-        form = SignUpForm()
+        signUpForm = SignUp()
 
-    return render(request, 'signup.html', {'signUpForm': form})
+    return render(request, 'signup.html', {'signUpForm': signUpForm})
 
 def userprofile(request):
     return render(request, 'userProfile.html')
@@ -35,22 +27,4 @@ def freshPage(request):
     return render(request, 'fresh.html')
 
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        print(username)
-        password = request.POST['password']
-        print(password)
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect('/spicy_memes') #succes redirect to the startpage
-        else:
-            print("no-succes")
-            return HttpResponseRedirect('/spicy_memes/loginPage') #wrong login-information: reload for the moment
-            
-
-
-    else:
-        form = LogInForm()
-
-    return render(request, 'login.html', {'LogInForm': form})
+    return render(request, 'login.html')
