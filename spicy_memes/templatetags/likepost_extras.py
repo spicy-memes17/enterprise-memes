2ff16512e57a6d8350c9825fec4d8fc90f4a9bcc
alexsteinmetz <alexandersteinmetz0@gmail.com>
2017-07-09 18:55:07 +0200

    post detail seite: Das Profilbild vom Poster ist jetzt dabei. Tags werden auch angezeigt. Spiciness Factor wurde geändert zu: Spiciness oder Mildness abhängig ob das Rating positiv oder negativ ist.

--- spicy_memes/templatetags/likepost_extras.py
+++ spicy_memes/templatetags/likepost_extras.py
@@ -9,7 +9,17 @@ def getPostLikes(post):
     return LikesPost.objects.filter(post=post).filter(likes=True).count() - LikesPost.objects.filter(
         post=post).filter(likes=False).count()
 
+@register.simple_tag
+def getnegPostLikes(post):
+    return LikesPost.objects.filter(post=post).filter(likes=True).count() - LikesPost.objects.filter(
+        post=post).filter(likes=False).count() * -1
 
+@register.simple_tag
+def positivlikes(post):
+    ret =True
+    if getPostLikes(post) < 0:
+        ret = False
+    return ret
 @register.filter
 def hasUpVotedPost(user, post):
     if (LikesPost.objects.filter(post=post).filter(user=user).filter(likes=True)):
